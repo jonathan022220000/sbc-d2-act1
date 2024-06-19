f520f4db579afff5eee94750ea1c4707fa502148
jonathan022220000 <jonathangenegabuas01@gmail.com>
2024-06-18 22:13:43 -0700

    Add files via upload

---
+++ sbc-d2-act1.py
@@ -0,0 +1,12 @@
+word = "summer bootcamp"
+#      01234567891011121314
+#     -141314121110987654321
+
+#
+print(word.title())
+#Lower Case
+print(word.title().replace("p","P"))
+print(word[7:11].replace("b","L"))
+print(word[11:15].replace("p","E"))
+print(word[12].upper() + word[5].upper())
+print(word.replace(" ","jonathan").isalpha())
